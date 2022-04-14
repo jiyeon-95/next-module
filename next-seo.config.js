de1311a059b39e47a6a58241db9e4699f42ccd17
Bona Park <jiyeonp59@gmail.com>
2022-04-14 16:17:21 +0900

    Apply next-seo with default template

---
+++ next-seo.config.js
@@ -0,0 +1,10 @@
+const defaultSEO = {
+  title: "Default",
+  titleTemplate: " %s | template of title",
+  description: "This is default config of NextSeo ",
+  openGraph: {
+    type: "website",
+  },
+};
+
+export default defaultSEO;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { DefaultSeo } from "next-seo";
+import nextSeoConfig from "../next-seo.config";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <DefaultSeo {...nextSeoConfig} />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
